dcb118ebac25c901abe07824ce98adb2bcea99b6
Martin Pöhlmann <poehlmann@cqse.eu>
2017-04-07 08:52:12 +0200

    Finetune Lichtenberg

--- src/main/java/com/mpdeimos/foodscraper/data/utum/Menu.java
+++ src/main/java/com/mpdeimos/foodscraper/data/utum/Menu.java
@@ -32,7 +32,7 @@ public class Menu implements IMenu
 	 * @see #getDishes()
 	 */
 	@Scrape(
-			value = ".menu-items .menu-item:has(.menu-item-title:matches(.*\\p{IsAlphabetic}+.*):not(:matches(\\*Pichler\\s+Bio\\s*-\\s*Fleisch)):not(:matches(Tageskarte)))",
+			value = ".menu-items .menu-item:has(.menu-item-title:matches(.*\\p{IsAlphabetic}+.*):not(:matches(\\*\\s*Pichler\\s+Bio\\s*-?\\s*Fleisch)):not(:matches(Tageskarte)))",
 			converter = DeepScrapeConverter.class)
 	public Dish[] dishes;
 
